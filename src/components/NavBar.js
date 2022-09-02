a57bf03a34ebc0cbc16c16320b255405fbe1ef2e
Lucasmartinez <garcia.lucaset36@gmail.com>
2022-09-02 15:51:33 -0300

    component nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+function NavBar() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg bg-nav">
+          <div className="container-fluid">
+            <a class="navbar-brand" href="#">
+            CDHamburguesa</a>
+            <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link fw-bold" href="#">Combos</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link fw-bold" href="#">Hamburguesa</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link fw-bold" href="#">Adicional</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link fw-bold" href="#">Bebida</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>  
+      </div>
+    );
+  }
+  
+  export default NavBar;
